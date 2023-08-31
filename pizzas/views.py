e514a8129e483f98a203e306f8e0296373b1c819
huseyintekin <huseyintekin10@gmail.com>
2023-08-31 10:13:18 +0200

    first commit

---
+++ pizzas/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+# Create your views here.
+def home(request):
+    context={
+    }
+    return render(request, 'pizzas/home.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
