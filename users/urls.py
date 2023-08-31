430b192a512ebb89c760043ace8870d3703ef587
huseyintekin <huseyintekin10@gmail.com>
2023-08-31 11:07:03 +0200

    self_user_form

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path,include
-from .views import home
-
+from django.urls import path, include
+from .views import register
 urlpatterns = [
-    #path('', home, name='home'),
+    path('register', register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm
 # Create your views here.
+def register(request):
+
+
+    #form=UserCreationForm()
+    form=UserForm()
+
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context={
+        'form':form
+    }
+    return render(request, 'users/register.html', context)
